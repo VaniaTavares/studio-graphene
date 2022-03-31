93a015ef468ac9b96ea21fdfa467f761443f855d
Vania Tavares <vntavares56@gmail.com>
2022-03-31 11:04:22 +0100

    section tracker

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   CommunitySection,
   LandingSection,
@@ -9,11 +9,55 @@ import {
 import { Navigation, SectionTracker } from "./Components";
 import "./App.css";
 
+const options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.8,
+};
+
 const App = () => {
+  const containerRef = useRef(null);
+  const [sectionsToWatch, setSectionsToWatch] = useState([]);
+  const [tracker, setTracker] = useState("");
+  const intersectionCallBack = (entries) => {
+    const watching = entries.find((entry) => entry.isIntersecting);
+
+    if (watching) setTracker(`#${watching.target.id}`);
+    else setTracker("");
+  };
+
+  useEffect(() => {
+    // const observer = new IntersectionObserver(intersectionCallBack, options);
+    const { current } = containerRef;
+    if (current) {
+      setSectionsToWatch((prevSet) => [
+        ...prevSet,
+        ...Array.from(current.children).splice(3),
+      ]);
+    }
+  }, [containerRef]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(intersectionCallBack, options);
+    if (sectionsToWatch.length === 4) {
+      sectionsToWatch.forEach((child) => {
+        observer.observe(child);
+      });
+    }
+
+    return () => {
+      if (sectionsToWatch.length) {
+        sectionsToWatch.forEach((child) => {
+          observer.unobserve(child);
+        });
+      }
+    };
+  }, [sectionsToWatch]);
+
   return (
-    <div className="App">
+    <div className="App" ref={containerRef}>
       <Navigation />
-      <SectionTracker />
+      <SectionTracker tracker={tracker} />
       <LandingSection />
       <CommunitySection />
       <LocationSection />
--- src/Components/SectionTracker/index.js
+++ src/Components/SectionTracker/index.js
@@ -4,23 +4,27 @@ import { navbarLinks } from "../../Constants/NavLinks";
 
 const treated = [...navbarLinks];
 treated.shift();
-const SectionTracker = () => {
+const SectionTracker = ({ tracker }) => {
   return (
     <div
       className="app__section-tracker"
-      style={{ height: "300px", position: "fixed", top: "50vh" }}
+      style={{ height: "300px", position: "fixed", top: "50vh", left: "1vw" }}
     >
       {treated.map((link) => (
-        <div key={link.id} style={{ width: "5vw", position: "relative" }}>
-          {link.id.substring(2)}
+        <div key={link.id} style={{ width: "50px", position: "relative" }}>
+          <p
+            className={
+              tracker === link.refLink
+                ? "p__section__active"
+                : "p__section__inactive"
+            }
+          >
+            {link.id.substring(2)}
+          </p>
           <div
-            style={{
-              height: "1px",
-              width: "50%",
-              backgroundColor: "black",
-              position: "absolute",
-              top: "50%",
-            }}
+            className={
+              tracker === link.refLink ? "line section__active" : "line"
+            }
           ></div>
         </div>
       ))}
