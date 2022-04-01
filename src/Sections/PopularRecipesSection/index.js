7cb11909c117b37492fd1a19814ff47e40d88202
Vania Tavares <vntavares56@gmail.com>
2022-04-01 16:50:50 +0100

    Popular Recipes styled

--- src/Sections/PopularRecipesSection/index.js
+++ src/Sections/PopularRecipesSection/index.js
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useRef } from "react";
+import {
+  BsFillArrowLeftSquareFill,
+  BsFillArrowRightSquareFill,
+} from "react-icons/bs";
+import { Loading, DisplayErrorMessage, DisplayCard } from "../../Components";
+import useRecipes from "../../Constants and Functions/useRecipes";
+
+import "./index.css";
 
 const PopularRecipesSection = () => {
+  const { isLoading, isError, recipes } = useRecipes();
+  const horizontalRef = useRef(null);
+  if (isLoading) return <Loading />;
+  else if (isError) return <DisplayErrorMessage />;
+
+  const handleScroll = (direction) => {
+    if (direction === "left") {
+      horizontalRef.current.scrollLeft -= 400;
+    } else {
+      horizontalRef.current.scrollLeft += 400;
+    }
+  };
   return (
-    <section id="popular-recipes" className="flex__start section__padding">
+    <section
+      id="popular-recipes"
+      className="flex__start section__padding"
+      style={{ position: "relative", top: "20vh" }}
+    >
       <h2 className="section__title">Popular Recipes</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas,
-        reprehenderit quis quibusdam rem totam eos expedita maxime sint
-        voluptatum, numquam tempore exercitationem?
-      </p>
+      <div className="app__recipes-container" ref={horizontalRef}>
+        {recipes.map((recipe) => (
+          <DisplayCard
+            recipe={recipe}
+            image={recipe.image}
+            title={recipe.title}
+            key={recipe.id + Math.random() * 10}
+            direction="column"
+          />
+        ))}
+      </div>
+      <div>
+        <BsFillArrowLeftSquareFill
+          onClick={() => handleScroll("left")}
+          fontSize={30}
+          color="#771e21"
+        />{" "}
+        <BsFillArrowRightSquareFill
+          onClick={() => handleScroll("right")}
+          color="#771e21"
+          fontSize={30}
+        />
+      </div>
     </section>
   );
 };
